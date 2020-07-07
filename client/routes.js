3bed3ded36a9932a5c2af224747f7f84095130fc
Jennifer Nugent <jmn2158@columbia.edu>
2020-07-07 16:48:25 -0400

    added route for all visitors for just a slash

--- client/routes.js
+++ client/routes.js
@@ -23,6 +23,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/" component={Signup} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
